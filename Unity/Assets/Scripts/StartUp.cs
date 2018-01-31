73b7da6defd3129f503a924a0cc0b0fcbac14d73
nlfosdic <nlfosdic@mtu.edu>
2018-01-30 19:32:09 -0500

    Note spawning attempt

--- Unity/Assets/Scripts/StartUp.cs
+++ Unity/Assets/Scripts/StartUp.cs
@@ -7,28 +7,49 @@ public class StartUp : MonoBehaviour
     public AudioSource[] Titles;
     public LevelInfo Info;
     public Note[] Notes;
+    public float maxSpawnTime;
 
 	private int noteCount;
 	private float noteSpeed;
+    private float spawnTime;
+    private AudioSource song;
 
     // Use this for initialization
     void Start()
     {
         Info.SetSongIndex(2); // use this line for testing
-        var song = Instantiate(Titles[Info.GetSongIndex()]);
-        song.PlayDelayed(2.1F);
+        song = Instantiate(Titles[Info.GetSongIndex()]);
+        song.PlayDelayed(2.9F);
 
         float beatDelay = 60.0F / (float)Info.GetSongBpm();
         float noteDelay = 0.0F;
 
-        noteCount = 300;
+        noteCount = 134;
         noteSpeed = 2.5F;
 
-        InvokeRepeating("SpawnNotes", noteDelay, beatDelay);
+        spawnTime = 0F;
+        //InvokeRepeating("SpawnNotes", noteDelay, beatDelay);
     }
 
-    void SpawnNotes()
+    void Update ()
     {
+        
+        spawnTime += Time.deltaTime;
+        if (spawnTime >= maxSpawnTime && song.time < 126)
+        {
+            spawnTime = 0;
+            SpawnNotes();
+        }
+        else
+        {
+            Application.Quit();
+        }
+    
+    }
+
+
+    void SpawnNotes()
+    {Debug.Log(song.time);
         var note = Instantiate(Notes[Random.Range(0, 4)]);
         //var note = Instantiate(Notes[0]);
         note.SetSpeed(noteSpeed);
