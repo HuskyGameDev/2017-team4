4b92257dd6675e54c95f3882e600344d8ffa9825
jkiger <jkiger@mtu.edu>
2018-02-06 19:19:10 -0500

    edit stats functions

--- Unity/Assets/Scripts/ShopManager.cs
+++ Unity/Assets/Scripts/ShopManager.cs
@@ -5,50 +5,57 @@ using UnityEngine.UI;
 
 public class ShopManager : MonoBehaviour
 {
-    // The stats associated with actions
-    // 0 - Defense (Resistance)
-    // 1 - Attack  (Force)
-    // 2 - Evade   (Dexterity)
-    public Text[] statCostTexts;
-    public PlayerStats playerStats;
-    public Text scoreDisplay;
+	// The stats associated with actions
+	// 0 - Defense (Resistance)
+	// 1 - Attack  (Force)
+	// 2 - Evade   (Dexterity)
+	public Text[] statCostTexts;
+	public PlayerStats playerStats;
+	public Text scoreDisplay;
 
-    private int[] statCosts;
+	public Text forceDisplayText;
+	public Text resDisplayText;
+	public Text dexDisplayText;
+
+	private int[] statCosts;
     
-    // Use this for initialization
-    void Start()
-    {
-        playerStats.AdjustScore(100);
-        statCosts = new int[3] { 10, 10, 10 };
+	// Use this for initialization
+	void Start ()
+	{
+		playerStats.AdjustScore (200);
 
-        statCostTexts[0].text = statCosts[0].ToString();
-        statCostTexts[1].text = statCosts[1].ToString();
-        statCostTexts[2].text = statCosts[2].ToString();
+		statCosts = new int[3] { 10, 10, 10 };
 
-        scoreDisplay.text = "Score: " + playerStats.GetScore().ToString();
-    }
+		statCostTexts [0].text = statCosts [0].ToString ();
+		statCostTexts [1].text = statCosts [1].ToString ();
+		statCostTexts [2].text = statCosts [2].ToString ();
 
-    public void PurchaseStat(int statIndex)
-    {
-        var currentScore = playerStats.GetScore();
-        if (currentScore < statCosts[statIndex])
-            return;
-        switch (statIndex)
-        {            
-            case 1:                
+		scoreDisplay.text = "Score: " + playerStats.GetScore ().ToString ();
+		forceDisplayText.text = "Force: "+playerStats.GetForceStat ().ToString();
+		resDisplayText.text = "Res: "+playerStats.GetResistanceStat ().ToString();
+		dexDisplayText.text = "Dex: "+playerStats.GetDexterityStat ().ToString();
+	}
+
+	public void PurchaseStat (int statIndex)
+	{
+		var currentScore = playerStats.GetScore ();
+		if (currentScore < statCosts [statIndex])
+			return;
+		switch (statIndex) {            
+		case 1:                
                 // buy attack (force)                    
-                playerStats.AdjustForceStat(1);
-                break;
-            case 2:
+			playerStats.AdjustForceStat (1);
+			break;
+		case 2:
                 // buy evasion (dexterity)
-                playerStats.AdjustDexterityStat(1);
-                break;
-            default:
+			playerStats.AdjustDexterityStat (1);
+			break;
+		default:
                 // buy defense (resistance)
-                playerStats.AdjustResistanceStat(1);
-                break;
-        }
-        playerStats.AdjustScore(-statCosts[statIndex]);
-        scoreDisplay.text = "Score: " + playerStats.GetScore().ToString();
-    }
+			playerStats.AdjustResistanceStat (1);
+			break;
+		}
+		playerStats.AdjustScore (-statCosts [statIndex]);
+		scoreDisplay.text = "Score: " + playerStats.GetScore ().ToString ();
+	}
 }
